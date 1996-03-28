821ddb5f26e76a36df98d0f97bccb5c5abceea8d
jcmurphy <jcmurphy>
1996-03-28 01:50:20 +0000

    fixed up the my() stuff

--- ARS.pm
+++ ARS.pm
@@ -36,7 +36,7 @@ require AutoLoader;
 require Config;
 
 @ISA = qw(Exporter DynaLoader);
-@EXPORT = qw(isa_int isa_float isa_string ars_LoadQualifier ars_Login ars_Logoff ars_GetListField ars_GetFieldByName ars_GetFieldTable ars_CreateEntry ars_DeleteEntry ars_GetEntry ars_GetListEntry ars_GetListSchema ars_GetListServer ars_GetActiveLink ars_GetCharMenuItems ars_GetSchema ars_GetField ars_simpleMenu ars_GetListActiveLink ars_SetEntry ars_perl_qualifier);
+@EXPORT = qw(isa_int isa_float isa_string ars_LoadQualifier ars_Login ars_Logoff ars_GetListField ars_GetFieldByName ars_GetFieldTable ars_CreateEntry ars_DeleteEntry ars_GetEntry ars_GetListEntry ars_GetListSchema ars_GetListServer ars_GetActiveLink ars_GetCharMenuItems ars_GetSchema ars_GetField ars_simpleMenu ars_GetListActiveLink ars_SetEntry ars_perl_qualifier ars_Export ars_GetListFilter ars_GetListEscalation ars_GetListCharMenu ars_GetListAdminExtension);
 
 bootstrap ARS;
 tie $main::ars_errstr, ARS::ERRORSTR;
@@ -54,13 +54,15 @@ $AR_EXECUTE_ON_LOOSE_FOCUS = 256;
 $AR_EXECUTE_ON_SET_DEFAULT = 512;
 $AR_EXECUTE_ON_QUERY =      1024;
 
+$field_entryId = 1;
+
 sub ars_simpleMenu {
     # merges all sub-menus into a single level menu
-    my $m = shift;
-    my $prepend = shift;
-    my @m = @$m;
-    my @ret, @submenu;
-    my $name, $val;
+    my($m) = shift;
+    my($prepend) = shift;
+    my(@m) = @$m;
+    my(@ret, @submenu);
+    my($name, $val);
     
     while (($name, $val, @m) = @m) {
 	if (ref($val)) {
@@ -75,6 +77,17 @@ sub ars_simpleMenu {
 	}
     }
     @ret;
+}
+
+sub pad_entryid {
+    my($c) = shift;
+    my($schema) = shift;
+    my($entry_id) = shift;
+    my($field);
+    
+    ($field = ars_GetField($c, $schema, $field_entryId)) ||
+	return undef;
+    return ("0"x($field->{limit}{maxLength}-length($entry_id))).$entry_id;
 }
 
 ARS::__ars_init();
