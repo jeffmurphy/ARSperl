5e18669c75719c3894c95a2161d7236bcce59197
mbeijen <mbeijen>
2007-08-02 14:48:21 +0000

    modified examples for ExecuteProcess and decodeStatusHistory

--- example/ars_ExecuteProcess.pl
+++ example/ars_ExecuteProcess.pl
@@ -1,15 +1,19 @@
 #!/usr/local/bin/perl
 #
-# $Header: /cvsroot/arsperl/ARSperl/example/ars_ExecuteProcess.pl,v 1.1 1997/07/23 18:21:29 jcmurphy Exp $
+# $Header: /cvsroot/arsperl/ARSperl/example/ars_ExecuteProcess.pl,v 1.2 2007/08/02 14:48:21 mbeijen Exp $
 #
 # NAME
 #   ars_ExecuteProcess.pl
 #
 # USAGE
 #   ars_ExecuteProcess.pl [server] [username] [password] ["process"]
+#    if you need to use a specified TCP port, export the ARTCPPORT environment variable
+#    with the TCP Port number
 # 
 # EXAMPLE
-#   ars_ExecuteProcess.pl arserver foobar barfoo "ls -l /"
+#   ars_ExecuteProcess.pl arserver user password "ls -l /" (if the server is on Unix)
+#  ars_ExecuteProcess.pl arserver user password " cmd /c dir" (if the server is on Win32)
+#  ars_ExecuteProcess.pl arserver user password Application-Generate-GUID
 #
 # DESCRIPTION
 #   Execute given command on remote arserver. Requires admin account to work.
@@ -18,6 +22,9 @@
 #   Jeff Murphy
 #
 # $Log: ars_ExecuteProcess.pl,v $
+# Revision 1.2  2007/08/02 14:48:21  mbeijen
+# modified examples for ExecuteProcess and decodeStatusHistory
+#
 # Revision 1.1  1997/07/23 18:21:29  jcmurphy
 # Initial revision
 #
@@ -26,7 +33,7 @@
 
 use ARS;
 
-$c = ars_Login(shift, shift, shift);
+$c = ars_Login(shift, shift, shift) || die "login: $ars_errstr";
 
 $b = shift;
 (($num, $str) = ars_ExecuteProcess($c, $b)) || print "ERR: $ars_errstr\n";
--- example/ars_decodeStatusHistory.pl
+++ example/ars_decodeStatusHistory.pl
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl
 #
-# $Header: /cvsroot/arsperl/ARSperl/example/ars_decodeStatusHistory.pl,v 1.2 1998/09/14 17:39:20 jcmurphy Exp $
+# $Header: /cvsroot/arsperl/ARSperl/example/ars_decodeStatusHistory.pl,v 1.3 2007/08/02 14:48:21 mbeijen Exp $
 #
 # NAME
 #   ars_decodeStatusHistory.pl [server] [username] [password] [schema] [eid]
@@ -13,6 +13,9 @@
 #   Jeff murphy
 #
 # $Log: ars_decodeStatusHistory.pl,v $
+# Revision 1.3  2007/08/02 14:48:21  mbeijen
+# modified examples for ExecuteProcess and decodeStatusHistory
+#
 # Revision 1.2  1998/09/14 17:39:20  jcmurphy
 # changed #!perl path
 #
@@ -23,48 +26,47 @@
 #
 
 use ARS;
+use warnings;
+use strict;
 
-($c = ars_Login(shift, shift, shift)) ||
+die "usage: ars_decodeStatusHistory.pl [server] [username] [password] [form] [entry]\n" unless ($#ARGV == 4);
+
+# Log in to the server
+(my $ctrl = ars_Login(shift, shift, shift)) ||
     die "login: $ars_errstr";
 
-($S, $E) = (shift, shift);
+(my $form, my $entry) = (shift, shift);
 
-(%f = ars_GetFieldTable($c, $S)) ||
-    die "GetFieldTable: $ars_errstr";
+# get the information for this entry. ars_GetEntry returns a hash of field ID's and its values.
+# in fact, we only are interested in this example in field 15 which contains the Status History.
+(my %entry = ars_GetEntry($ctrl, $form, $entry, 15)) ||
+    die "GetEntry: $ars_errstr (no entry $entry in form $form ?)";
 
-(%e = ars_GetEntry($c, $S, $E)) ||
-    die "GetEntry: $ars_errstr (no matching entry?)";
+# load the field information for field 7 (Status) so we can determine what the names are for 
+# the different statuses.
+(my $field_info = ars_GetField($ctrl, $form, 7)) ||
+    die "GetField: $ars_errstr ";
 
-($fh = ars_GetField($c, $S, $f{'Status'})) ||
-    die "GetField: $ars_errstr (no Status field in this schema?)";
+# retrieving all status values for enumerated field Status
+my @enum_vals = @{$field_info->{limit}{enumLimits}{regularList}};
 
-if($fh->{dataType} ne "enum") {
-    die "'Status' field is not an enum.\n";
+print "Status values: ".join(', ', @enum_vals)."\n";
+
+#The Status History field is always field number 15.
+my @status_values = ars_decodeStatusHistory($entry{15});
+my $i;
+# loop trough the created Status Values array, which is an array that contains hash refs
+# we'll print the Status name with it, which we retrieved earlier using ars_GetField.
+foreach (@status_values) {
+    print $enum_vals[$i++].": \n";
+	if ($_->{USER}) { 
+	print "\tUSER: ".$_->{USER}."\n";
+    print "\tTIME: ".localtime($_->{TIME})."\n"; # with localtime we convert Epoch to human-readable
+	} else {
+    print "\tNo Status History entry\n"; 
+    }	
 }
-
-@enumvals = @{$fh->{limit}};
-
-print "Status values: ".join(',', @enumvals)."\n";
-
-if(!defined($f{'Status-History'})) {
-    die "no Status-History field?\n";
-}
-
-if(!defined($e{$f{'Status-History'}})) {
-    die "no Status-History field values to decode.\n";
-}
-
-@sv = ars_decodeStatusHistory($e{$f{'Status-History'}});
-
-$i = 0;
-foreach (@sv) {
-    print $enumvals[$i++].": \n";
-    print "\tUSER: ".$_->{USER}."\n";
-    print "\tTIME: ".localtime($_->{TIME})."\n";
-}
-
-ars_Logoff($c);
+# log off nicely.
+ars_Logoff($ctrl);
 
 exit 0;
-
-
